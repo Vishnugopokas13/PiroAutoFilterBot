dd5160bb1590c1854cf40b05b0305de0f56d6d25
ᴠɪꜱʜɴᴜ ᴀ <99662065+Vishnuvishnu2naka@users.noreply.github.com>
2023-06-13 00:14:54 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = 
+API_HASH = ''
+BOT_TOKEN = ''
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -12,7 +12,8 @@ USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
 ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+CHANNELS = []
+            
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
